03f74793a6df7d60c3f565adf5c293ce343cc647
Student <league@iMac-18.attlocal.net>
2022-11-14 16:52:14 -0800

    thread_pool, synchronized_swimming

--- src/_04_Thread_Pool/WorkQueue.java
+++ src/_04_Thread_Pool/WorkQueue.java
@@ -1,7 +1,12 @@
 package _04_Thread_Pool;
 
+import java.lang.Thread.State;
+import java.util.ArrayDeque;
+
 public class WorkQueue implements Runnable {
+	private ArrayDeque<Job> jobQueue = new ArrayDeque<Job>();
 	private Thread[] threads;
+	private volatile boolean isRunning = true;
 	
 	public WorkQueue() {
 		int totalThreads = Runtime.getRuntime().availableProcessors() - 1;
@@ -17,9 +22,61 @@ public class WorkQueue implements Runnable {
 		return threads.length;
 	}
 	
+	public boolean preformJob() {
+		Job j = null;
+		synchronized(jobQueue) {
+			if(!jobQueue.isEmpty()) {
+				j = jobQueue.remove();
+			}
+		}
+		
+		if (j != null) {
+			j.preform();
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void shutdown() {
+		completeAllJobs();
+		isRunning = false;
+		synchronized (jobQueue) {
+			jobQueue.notifyAll();
+		}
+	}
+	public void addJob(Job j) {
+		synchronized (jobQueue) {
+			jobQueue.add(j);
+			jobQueue.notify();
+		}
+	}
+	
+	public void completeAllJobs() {
+		while(!jobQueue.isEmpty()) {
+			preformJob();
+		}
+		
+		for (int i = 0; i < threads.length; i++) {
+			if (threads[i].getState() != State.WAITING) {
+				i = -1;
+			}
+		}
+	}
+	
 	@Override
 	public void run() {
-		
+		while(isRunning) {
+			if (!preformJob()) {
+				synchronized (jobQueue) {
+					try {
+						jobQueue.wait();
+					}catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 	}
 
 }
