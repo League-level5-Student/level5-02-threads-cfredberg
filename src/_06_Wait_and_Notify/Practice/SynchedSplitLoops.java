b76ffa194252e038601bf3ad17b0f78552bd38b7
cfredberg <53453343+cfredberg@users.noreply.github.com>
2022-11-21 12:24:54 -0800

    need to finish recipe 6

--- src/_06_Wait_and_Notify/Practice/SynchedSplitLoops.java
+++ src/_06_Wait_and_Notify/Practice/SynchedSplitLoops.java
@@ -17,17 +17,30 @@ printed in order.
 
 public class SynchedSplitLoops {
 	static int counter = 0;
+	static Object threadLock = new Object();
 	
 	public static void main(String[] args) {
 		Thread t1 = new Thread(() -> {
 			for(int i = 0; i < 100000; i++) {
 				counter++;
+				threadLock.notify();
+				try {
+					threadLock.wait();
+				} catch (InterruptedException e) {
+					System.out.println("error!");
+				}
 			}
 		});
 		
 		Thread t2 = new Thread(() -> {
 			for(int i = 0; i < 100000; i++) {
+				try {
+					threadLock.wait();
+				} catch (InterruptedException e) {
+					System.out.println("error!");
+				}
 				System.out.println(counter);
+				threadLock.notify();
 			}
 		});
 		
